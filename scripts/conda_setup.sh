7681d9b28ad9981b354a7bf8e7d94166d0246b88
dinhanhx <dinhanhx@gmail.com>
2025-10-15 17:30:18 +0700

    Add scripts

---
+++ scripts/conda_setup.sh
@@ -0,0 +1,3 @@
+conda create -n mlqa-tsr python=3.10 pip
+conda install nvidia/label/cuda-12.4.1::cuda
+conda install nvidia/label/cuda-12.4.1::cuda-toolkit
---
+++ scripts/pip_setup.sh
@@ -0,0 +1,18 @@
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+
+pip install 'numpy<2' einops matplotlib scipy polars
+
+pip install transformers
+pip install timm
+pip install -U "huggingface_hub[cli]" 
+pip install open_clip_torch
+
+pip install openai
+pip install qdrant-client
+pip install elasticsearch==8.17.2
+pip install pymongo
+
+pip install ipykernel
+
+pip install "html-to-markdown[lxml]"
+pip install python-dotenv
